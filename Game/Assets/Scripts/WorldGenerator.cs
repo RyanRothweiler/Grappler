98c78da605b9271a72f5584d60d2557b119f3b1b
RyanRothweiler <ryanrothweiler@gmail.com>
2015-02-21 16:39:42 -0800

    backing up the pieces

---
+++ Game/Assets/Scripts/WorldGenerator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldGenerator : MonoBehaviour
+{
+	public static WorldGenerator instance;
+
+	void Start () 
+	{
+		instance = this;	
+	}
+	
+	void Update () 
+	{
+	
+	}
+
+	public void GenerateWorld()
+	{
+		WorldPieceController[] pieceLibrary = Resources.FindObjectsOfTypeAll(typeof(WorldPieceController)) as WorldPieceController[];
+
+		foreach(WorldPieceController piece in pieceLibrary)
+		{
+			if (piece.active)
+			{
+				DestoryImmediately(piece);
+			}
+		}
+	}
+}
